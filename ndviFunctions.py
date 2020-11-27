a7f329ae44e03f76cc7e6013a5c2583e5acb05a8
ruchi <ruchirawperera@gmail.com>
2020-11-27 18:54:01 -0500

    firebase not working yet

---
+++ ndviFunctions.py
@@ -0,0 +1,107 @@
+import numpy as np #arrays and math
+import cv2 #opencv library
+from google.cloud import storage
+
+#NDVI Calculation
+#Input: an RGB image frame from infrablue source (blue is blue, red is pretty much infrared)
+#Output: an RGB frame with equivalent NDVI of the input frame
+def NDVICalc(original):
+    "This function performs the NDVI calculation and returns an RGB frame)"
+    lowerLimit = 5 #this is to avoid divide by zero and other weird stuff when color is near black
+
+    #First, make containers
+    oldHeight,oldWidth = original[:,:,0].shape; 
+    ndviImage = np.zeros((oldHeight,oldWidth,3),np.uint8) #make a blank RGB image
+    ndvi = np.zeros((oldHeight,oldWidth),np.int) #make a blank b/w image for storing NDVI value
+    red = np.zeros((oldHeight,oldWidth),np.int) #make a blank array for red
+    blue = np.zeros((oldHeight,oldWidth),np.int) #make a blank array for blue
+
+    #Now get the specific channels. Remember: (B , G , R)
+    red = (original[:,:,2]).astype('float')
+    blue = (original[:,:,0]).astype('float')
+
+    #Perform NDVI calculation
+    summ = red+blue
+    summ[summ<lowerLimit] = lowerLimit #do some saturation to prevent low intensity noise
+
+    ndvi = (((red-blue)/(summ)+1)*127).astype('uint8')  #the index
+
+    redSat = (ndvi-128)*2  #red channel
+    bluSat = ((255-ndvi)-128)*2 #blue channel
+    redSat[ndvi<128] = 0; #if the NDVI is negative, no red info
+    bluSat[ndvi>=128] = 0; #if the NDVI is positive, no blue info
+
+
+    #And finally output the image. Remember: (B , G , R)
+    #Red Channel
+    ndviImage[:,:,2] = redSat
+
+    #Blue Channel
+    ndviImage[:,:,0] = bluSat
+
+    #Green Channel
+    ndviImage[:,:,1] = 255-(bluSat+redSat)
+
+    return ndviImage
+
+
+
+#DVI Calculation
+#Input: an RGB image frame from infrablue source (blue is blue, red is pretty much infrared)
+#Output: an RGB frame with equivalent DVI of the input frame
+def DVICalc(original):
+    "This function performs the DVI calculation and returns an RGB frame)"
+
+    #First, make containers
+    oldHeight,oldWidth = original[:,:,0].shape; 
+    dviImage = np.zeros((oldHeight,oldWidth,3),np.uint8) #make a blank RGB image
+    dvi = np.zeros((oldHeight,oldWidth),np.int) #make a blank b/w image for storing DVI value
+    red = np.zeros((oldHeight,oldWidth),np.int) #make a blank array for red
+    blue = np.zeros((oldHeight,oldWidth),np.int) #make a blank array for blue
+
+    #Now get the specific channels. Remember: (B , G , R)
+    red = (original[:,:,2]).astype('float')
+    blue = (original[:,:,0]).astype('float')
+
+    #Perform DVI calculation
+    dvi = (((red-blue)+255)/2).astype('uint8')  #the index
+
+    redSat = (dvi-128)*2  #red channel
+    bluSat = ((255-dvi)-128)*2 #blue channel
+    redSat[dvi<128] = 0; #if the NDVI is negative, no red info
+    bluSat[dvi>=128] = 0; #if the NDVI is positive, no blue info
+
+
+    #And finally output the image. Remember: (B , G , R)
+    #Red Channel
+    dviImage[:,:,2] = redSat
+
+    #Blue Channel
+    dviImage[:,:,0] = bluSat
+
+    #Green Channel
+    dviImage[:,:,1] = 255-(bluSat+redSat)
+
+    return dviImage
+
+
+
+    #uploading to the cloud 
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    #"""Uploads a file to the bucket."""
+    
+    bucket_name = "your-bucket-name"
+    source_file_name = "local/path/to/file"
+    destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
---
+++ ndvi_testCode.py
@@ -0,0 +1,72 @@
+#-------------------------------------------
+#----------------Main Function--------------
+#-------------------------------------------
+import numpy as np #arrays and math
+import cv2 #opencv library
+import datetime
+import os
+import pyrebase
+from ndviFunctions import NDVICalc, DVICalc
+
+config{
+
+
+}
+firebase = pyrebase.initialize_app(config)
+storage = firebase.storage()
+
+path_on_cloud = "images/example.jpg"
+path_local = "/screenshots/example.jpg"
+storage.child(path_on_cloud).put(path_local)
+
+
+cv2.namedWindow("preview NDVI")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+    height = vc.get(3) #get height
+    width = vc.get(4) #get width
+    #Text Related
+    x = int(width/2)
+    y = int(2*height/3) 
+    text_color = (255,255,255) #color as (B,G,R)
+    font = cv2.FONT_HERSHEY_PLAIN
+    thickness = 2
+    font_size = 2.0
+else:
+    rval = False
+
+while rval:
+    ndviImage = NDVICalc(frame)
+    #dviImage = DVICalc(frame)
+
+    cv2.putText(frame, "Raw Image", (x,y), font, font_size, text_color, thickness, lineType=cv2.LINE_AA)
+    cv2.putText(ndviImage, "NDVI Image", (x,y), font, font_size, text_color, thickness, lineType=cv2.LINE_AA)
+    #cv2.putText(dviImage, "DVI Image", (x,y), font, font_size, text_color, thickness, lineType=cv2.LINE_AA)
+
+    #newFrame = np.concatenate((ndviImage,dviImage,frame),axis=1)
+    newFrame = np.concatenate((ndviImage,frame),axis=1)
+    cv2.imshow("preview NDVI", newFrame)
+
+    rval, frame = vc.read()
+
+
+    key = cv2.waitKey(1)&0xFF #get a key press
+    if key == ord('q'): #q for quitting
+        break
+    elif key == ord('p'): #p for printscreen
+        
+        path = 'C:/Users/ruchi/Desktop/CapstoneImageProcessing/NDVIcodes/screenshots'
+
+        curtime = datetime.datetime.now()
+        formattedTime = curtime.strftime("%Y%m%d-%H-%M-%S.jpg")
+        print ('filename:%s'%formattedTime)
+        cv2.imwrite(os.path.join(path,formattedTime),newFrame)
+        print ("Screenshot taken!")
+
+# When everything done, release the capture
+vc.release()
+cv2.destroyAllWindows()
+
+
